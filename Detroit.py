7c1cb7df13af466c292b34a2ba636517534ee7dd
Detroit2 <75081267+Detroit2@users.noreply.github.com>
2020-11-27 08:41:16 +0530

    Add files via upload

--- Detroit.py
+++ Detroit.py
@@ -46,21 +46,41 @@ async def _8ball(ctx, question):
 
 @client.command()
 async def info(ctx):
-	myEmbed = discord.Embed(tittle="Current Version", color=0x0000FF)
-	myEmbed.add_field(name="Version Code:", value="v1.0.0", inline=False)
-	myEmbed.add_field(name="Date Released:", value="November 25th, 2020", inline=False)
-	myEmbed.add_field(name="Author:", value="DarkLord#2292", inline=False)
-	myEmbed.set_author(name="Detroid")
+	n = random.randint(0,2)
+	if n == 0:
+		myEmbed = discord.Embed(tittle="Current Version                                           ", color=0x00FFFF)
+		myEmbed.add_field(name="Version Code:", value="v1.0.0                                     ", inline=False)
+		myEmbed.add_field(name="Date Released:", value="November 25th, 2020                       ", inline=False)
+		myEmbed.add_field(name="Author:", value="DarkLord#2292                                    ", inline=False)
+		myEmbed.set_author(name="Detroid                                                          ")
 	
-	await ctx.send(embed=myEmbed)
+		await ctx.send(embed=myEmbed)
+
+	if n == 1:
+		myEmbed = discord.Embed(tittle="Current Version                                           ", color=0xFFB6C1)
+		myEmbed.add_field(name="Version Code:", value="v1.0.0                                     ", inline=False)
+		myEmbed.add_field(name="Date Released:", value="November 25th, 2020                       ", inline=False)
+		myEmbed.add_field(name="Author:", value="DarkLord#2292                                    ", inline=False)
+		myEmbed.set_author(name="Detroid                                                          ")
+	
+		await ctx.send(embed=myEmbed)
+
+	if n == 2:
+		myEmbed = discord.Embed(tittle="Current Version                                           ", color=0x00FF00)
+		myEmbed.add_field(name="Version Code:", value="v1.0.0                                     ", inline=False)
+		myEmbed.add_field(name="Date Released:", value="November 25th, 2020                       ", inline=False)
+		myEmbed.add_field(name="Author:", value="DarkLord#2292                                    ", inline=False)
+		myEmbed.set_author(name="Detroid                                                          ")
+	
+		await ctx.send(embed=myEmbed)		
 
 @client.command()
 async def help(ctx):
-	helpEmbed = discord.Embed(tittle="Help Menu", color=0x000000)
-	helpEmbed.set_author(name="Help Menu",icon_url="https://i.pinimg.com/originals/fd/a1/3b/fda13b9d6d88f25a9d968901d319216a.jpg")
+	helpEmbed = discord.Embed(tittle="Help Menu                                                   ", color=0x000000)
+	helpEmbed.set_author(name="Help Menu                                                          ")
 	helpEmbed.set_thumbnail(url="https://i.pinimg.com/originals/fd/a1/3b/fda13b9d6d88f25a9d968901d319216a.jpg")
-	helpEmbed.add_field(name="Moderation Command", value=";clear", inline=False)
-	helpEmbed.add_field(name="Miscellaneous commands", value=";ping, ;8ball, ;info")
+	helpEmbed.add_field(name="Moderation Command                                                  ", value=";clear", inline=False)
+	helpEmbed.add_field(name="Miscellaneous commands", value=";ping, ;8ball, ;info                ")
 
 	await ctx.send(embed=helpEmbed)
 
