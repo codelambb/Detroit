16e4f85931655bdb7c47b0ae0f5ecdf968a8d707
Detroit2 <75081267+Detroit2@users.noreply.github.com>
2020-11-29 22:12:05 +0530

    Add files via upload

--- Detroit.py
+++ Detroit.py
@@ -241,30 +241,106 @@ async def unmute(ctx, member: discord.Member):
 	await ctx.send(f'Unmuted {member.mention}')
 	await member.send(f'You have been unmuted from the server {guild.name}')
 
+def convert(time):
+	pos = ["s","m","h","d"]
+
+	time_dict = {"s" : 1, "m" : 60, "h" : 3600, "d" : 3600*24}
+
+	unit = time[-1]
+
+	if unit not in pos:
+		return -1
+	try:
+		val = int(time[:-1])
+	except:
+		return -2
+
+	return val * time_dict[unit]
+
 #giveaway command
 @client.command()
-async def gstart(ctx, mins : int, *, prize: str):
-	embed = discord.Embed(tittle= "Giveaway!", description=f'{prize}', color= ctx.author.color)
+@commands.has_permissions(manage_roles=True, administrator=True, manage_channels=True, manage_server=True)
+async def giveaway(ctx):
+	await ctx.send("Lets start with giveaway! Answer these questions within 15 seconds")
 
-	end = datetime.datetime.utcnow() + datetime.timedelta(seconds=mins*60)
+	questions = ["Which channel should it be hosted in?",
+				"What should be the duration of the giveaway? (s|m|h|d)",
+				"What is the prize of the giveaway?"]
 
-	embed.add_field(name="Ends at:", value=f"{end} UTC")
-	embed.set_footer(text = "Ends {mins} minutes from now")
+	answers = []
 
-	my_msg = await ctx.send(embed=embed)
+	def check(m):
+		return m.author == ctx.author and m.channel == ctx.channel
+
+	for i in questions:
+		await ctx.send(i)
+
+		try:
+			msg = await client.wait_for('message', timeout=15.0, check=check)
+		except asyncio.TimeoutError:
+			await ctx.send('You didnt\'t answer in time, please be quicker next time!')
+			return
+		else:
+			answers.append(msg.content)
+
+	try:
+		c_id = int(answers[0][2:-1])
+	except:
+		await ctx.send(f"You didn't mention a channel properly. Do it like this {ctx.channel.mention} next time.")
+		return
+
+	channel = client.get_channel(c_id)
+
+	time = convert(answers[1])
+	if time == -1:
+		await ctx.send(f"You didn't answer the time with proper unit. Use (s|m|h|d)")
+		return
+	elif time == -2:
+		await ctx.send(f"The time must be an integer. Please eneter an integer next time.")
+		return
+	prize = answers[2]
+
+	await ctx.send(f"The Giveaway will be in {chann.mention} and it will last {time} seconds!")
+
+
+	embed = discord.Embed(tittle = "Giveaway!", description = f"{prize}", color = ctx.author.color)
+	embed.add_field(name = "Hosted by:", value= ctx.author.mention)
+	embed.set_footer(text = f"Ends {answers[1]} from now!")
+
+	my_msg = await channel.send(embed=embed)
 
 	await my_msg.add_reaction("🎉")
 
-	await asyncio.sleep(mins)
+	await asyncio.sleep(time)
 
-	new_msg = await ctx.channel.fetch_message(my_msg.id)
+	new_msg = await channel.fetch_message(my_msg.id)
 
 	users = await new_msg.reactions[0].users().flatten()
 	users.pop(users.index(client.user))
 
 	winner = random.choice(users)
 
-	await ctx.send(f"Congratulations! {winner.mention} won {prize}!")
+	await channel.send(f"Congratulation! {winner.mention} won {prize}!")
+
+@client.command()
+@commands.has_permissions(manage_roles=True, administrator=True, manage_channels=True, manage_server=True)
+async def reroll(ctx, channel : discord.Channel, id_ : int):
+	try:
+		new_msg = await channel.fetch_message(id_)
+	except:
+		await ctx.send("The id was entered incorrectly")
+		return
+		
+	users = await new_msg.reactions[0].users().flatten()
+	users.pop(users.index(client.user))
+
+	winner = random.choice(users)
+
+	await channel.send(f"Congratulation! {winner.mention} won {prize}!")
+
+
+	
+
 
 #all the errors
 
