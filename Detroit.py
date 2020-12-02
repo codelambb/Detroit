26bbc95a0b5484091c4a041467c2b7d7da9d67bc
Detroit2 <75081267+Detroit2@users.noreply.github.com>
2020-12-02 09:54:38 +0530

    Add files via upload

--- Detroit.py
+++ Detroit.py
@@ -220,7 +220,7 @@ async def avatar(ctx, *, member: discord.Member=None):
     await ctx.send(userAvatar)
 
 #mute command
-@commands.command()
+@commands.command(aliases=['mute','m'])
 @commands.has_permissions(manage_roles=True, administrator=True)
 async def mute(self, ctx, members: commands.Greedy[discord.Member],
                mute_minutes: typing.Optional[int] = 0, *, reason: str = "None"):
